70eaca8ab2df996b824700433210577730ab132f
Marcel Degas <marcel.degas@gmail.com>
2016-01-22 16:04:34 -0800

    Initiallized yeoman MEAN shell with couchDB as server

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name expensesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the expensesApp
+ */
+angular.module('expensesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
